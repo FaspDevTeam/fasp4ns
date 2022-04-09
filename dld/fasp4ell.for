df6d0ddb339d87de34b6c6511bdb6988ded332d8
zhangcs <zhangchensong@gmail.com>
2022-04-09 21:13:59 +0800

    Update DLD interface fasp4ell

--- dld/fasp4ell.for
+++ dld/fasp4ell.for
@@ -1,91 +1,116 @@
+ccccc   last edit time: 202204081401
       implicit real*8 (a-h,o-z)
-      integer,allocatable::NUMCOL(:),na(:)
+      integer,allocatable::numcol(:),na(:)
       double precision,allocatable::a(:),f(:),u(:)
-      character*12 fname
-
-      maxt=20000000
-
-      CALL getarg(1, fname)
-      OPEN (2,FILE=fname,FORM='UNFORMATTED',STATUS='OLD')
-      READ(2) NUMEL,NEQ
-      CLOSE(2)
+c========================  new add begin
+      real*8 :: estif(40000)
+      real*8,allocatable :: jnz(:)
+      character*50 fname
+c======================== new add end
+c========================  new add begin 
+c      maxt=20000000
+c======================== new add end 
+      open (2,file=' ',form='unformatted',status='old')
+      read(2) numel,neq
+      close(2)
 
       neq1=neq+1
       maxcol=maxt/neq
 
-      ALLOCATE(numcol(neq1),STAT=ierror)
-      ALLOCATE(na(maxt),STAT=ierror)
-      ALLOCATE(A(maxt),STAT=ierror)
-      ALLOCATE(f(neq),STAT=ierror)
-      ALLOCATE(u(neq),STAT=ierror)
+      allocate(numcol(neq1),stat=ierror)
+      allocate(f(neq),stat=ierror)
+      allocate(u(neq),stat=ierror)
+c========================  new add begin 
+      allocate(jnz(neq1),stat=ierror)  
+
 
       do i=1,neq1
          numcol(i)=0
       enddo
-      do i=1,maxt
-         na(i)=0
-         a(i)=0.d0
-      enddo
+c      do i=1,maxt
+c         na(i)=0
+c         a(i)=0.d0
+c      enddo
 
-C.......OPEN F FILE
-        OPEN(2,FILE='f',FORM='UNFORMATTED',STATUS='OLD')
-      READ (2) (F(I),I=1,NEQ)
-      CLOSE (2)
-C     WRITE(*,*) ' F ='
-C     WRITE(*,7) (F(I),I=1,NEQ)
-      DO 400 I=1,NEQ
-400   U(I)=0.0
+c.......open f file
+        open(2,file=' ',form='unformatted',status='old')
+      read (2) (f(i),i=1,neq)
+      close (2)
+c     write(*,*) ' f ='
+c     write(*,7) (f(i),i=1,neq)
+      do 400 i=1,neq
+400   u(i)=0.0
  
-C.......OPEN SM FILE
-C.......OPEN EINFORM & ESTIFF FILE
-        OPEN (1,FILE='einform',FORM='UNFORMATTED',STATUS='OLD')
-        OPEN (2,FILE='estiff',FORM='UNFORMATTED',STATUS='OLD')
- 
-        CALL ACLH(NEQ1,NUMEL,NUMCOL,NA,1,MAXCOL)
-        CALL BCLH(NEQ,NUMCOL,NA,MAXCOL)
-        MAXA=NUMCOL(NEQ+1)
- 
-        IF (MAXA.GT.MAXT) THEN
-          WRITE(*,*) 'MATRIX A EXCEED CORE MEMORY....',MAXT,maxa
-          STOP 0000
-        ENDIF
- 
-C      WRITE (*,*) 'NUMCOL ='
-C      WRITE (*,*) (NUMCOL(I),I=1,NEQ+1)
-C      WRITE (*,*) 'NA ='
-C      WRITE (*,*) (NA(I),I=1,MAXA)
-      REWIND(1)
-      CALL ADDA(NA,A,NUMCOL,NEQ,MAXA,NUMEL,1,2)
-c      WRITE (*,*) 'A ='
-c      WRITE (*,*) (A(I),I=1,MAXA)
-      CLOSE (1)
-      CLOSE (2)
+c.......open sm file
+c.......open einform & estiff file
+        open (1,file=' ',form='unformatted',status='old')
+        open (2,file=' ',form='unformatted',status='old')
+c========================  new add begin 
+        call acln(neq,numel,numcol,1)
+        maxt = int(numcol(neq+1)*1.1)
+        allocate(na(maxt),stat=ierror)
+        allocate(a(maxt),stat=ierror) 
+        do i=1,maxt
+          na(i)=0
+          a(i)=0.d0
+        enddo      
+        print*,maxt
+c======================== new add end 
 
-c      OPEN (2,FILE='csrmat_FE.dat',FORM='FORMATTED',STATUS='unknown')
+        call aclh(neq,numel,jnz,numcol,na,1,maxt)
+        call bclh(neq,numel,jnz,numcol,na,maxt)
+c======================== 
+
+        maxa=numcol(neq+1)
+ 
+        if (maxa.gt.maxt) then
+          write(*,*) 'matrix a exceed core memory....',maxt,maxa
+          stop 0000
+        endif
+ 
+c      write (*,*) 'numcol ='
+c      write (*,*) (numcol(i),i=1,neq+1)
+c      write (*,*) 'na ='
+c      write (*,*) (na(i),i=1,maxa)
+      rewind(1)
+      call adda(na,a,numcol,estif,neq,maxa,numel,1,2)
+
+c      write (*,*) 'a ='
+c      write (*,*) (a(i),i=1,maxa)
+      close (1)
+      close (2)
+
+c      open (2,file='csrmat_fe.dat',form='formatted',status='unknown')
 c      write(2,*) neq
-c      write(2,*) (numcol(I),I=1,neq+1)
-c      write(2,*) (na(I),I=1,maxa)
-c      write(2,*) (a(I),I=1,maxa)
-c      CLOSE(2)
-c      OPEN (2,FILE='rhs_FE.dat',FORM='FORMATTED',STATUS='unknown')
+c      write(2,*) (numcol(i),i=1,neq+1)
+c      write(2,*) (na(i),i=1,maxa)
+c      write(2,*) (a(i),i=1,maxa)
+c      close(2)
+c      open (2,file='rhs_fe.dat',form='formatted',status='unknown')
 c      write(2,*) neq
-c      write(2,*) (f(I),I=1,neq)
-c      CLOSE(2)
+c      write(2,*) (f(i),i=1,neq)
+c      close(2)
 
-      tol=1.d-06
-      maxit=100
-      info=5
+      forall (i=1:maxa+1) na(i) = na(i) - 1
+
+
+      tol=1.d-20
+      maxit=1000
+      info=3
       call fasp_fwrapper_dcsr_krylov_amg(neq,maxa,numcol,na,a,f,u,tol,
      *maxit,info)
 
-      OPEN (3,FILE='u',FORM='UNFORMATTED',STATUS='unknown')
-      WRITE (3) (U(I),I=1,NEQ)
-      CLOSE (3)
+      open (3,file=' ',form='unformatted',status='unknown')
+      write (3) (u(i),i=1,neq)
+      close (3)
 
-      DEALLOCATE(numcol)
-      DEALLOCATE(na)
-      DEALLOCATE(a)
-      DEALLOCATE(f)
-      DEALLOCATE(u)
+      deallocate(numcol)
+      deallocate(f)
+      deallocate(u)
+      deallocate(jnz)
+      deallocate(na)
+      deallocate(a)
 
-      END
+      
+
+      end program
